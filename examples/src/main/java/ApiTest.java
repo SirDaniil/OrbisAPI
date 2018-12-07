c5910d14d1a638f9351420b057c4c895badbdff9
Daniil S <daniil@orbisfn.com>
2018-12-06 22:30:34 -0500

    research

--- examples/src/main/java/ApiTest.java
+++ examples/src/main/java/ApiTest.java
@@ -23,19 +23,20 @@ public class ApiTest
                 OrbisAPI api = new OrbisAPI();
                 api.setHostname(props.getProperty("hostname"));
                 api.setCredentials(new PublicKeyCredentials(props.getProperty("key.file")));
+                System.out.println(api.getQuotes("amzn").toString(2));
 
-                System.out.println(((JSONArray)api.get(OrbisAPI.Endpoint.ResearchMarketDates)).toString(2));
+                /*System.out.println(((JSONArray)api.get(OrbisAPI.Endpoint.ResearchMarketDates)).toString(2));
                 System.out.println(((JSONObject)api.get(OrbisAPI.Endpoint.ResearchMarketDateLastOpen)).toString(2));
-                System.out.println(((JSONObject)api.get(OrbisAPI.Endpoint.ResearchMarketDateCheck, "date", "9/25/2018")).toString(2));
+                System.out.println(((JSONObject)api.get(OrbisAPI.Endpoint.ResearchMarketDateCheck, "date", "9/25/2018")).toString(2));*/
                 //System.out.println(((JSONObject)api.get(OrbisAPI.Endpoint.Research, "{symbol}", "baba")).toString(2));
                 /*System.out.println(api.getCorporateActionTypes());
                 System.out.println(api.corporateActionSearch(CorporateActionSearch.Builder().type("Dividend")).toString(2));*/
                 //System.out.println(api.news(NewsFilter.Builder().filter( new FilterKey(FilterKey.Provider).add(Providers.Sec).add(Providers.TenkWiz) )).toString(2));
-                //System.out.println(api.getChartHistorical("AAPL", "1y").toString(2));
+                //System.out.println(api.getChartHistorical("AAPL", "1m").toString(2));
                 //System.out.println(api.news("AAPL"));
                 /*System.out.println(api.news(NewsFilter.Builder().filter(FilterKey.Provider.add(Providers.Sec).exclude())).toString(2));
                 System.out.println(api.getFundamentals("IncomeStatement", "MSFT").toString(2));
-                System.out.println(api.getQuotes("amzn").toString(2));
+
                 System.out.println(api.getFundamentalTypes().toString(2));
                 System.out.println(api.getAdrsTop10Defaults().toString(2));
                 System.out.println(api.getAdrsTop10(AdrRequest.Builder().country("CAN")).toString(2));
