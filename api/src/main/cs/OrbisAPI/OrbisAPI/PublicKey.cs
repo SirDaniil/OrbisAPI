bbff401edccb7491890150be5c4595328717185f
Daniil S <daniil@orbisfn.com>
2019-01-18 22:30:00 -0500

    C# API sample

---
+++ api/src/main/cs/OrbisAPI/OrbisAPI/PublicKey.cs
@@ -0,0 +1,53 @@
+﻿using BlackyPaw.Crypto;
+using Jose;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace OrbisAPI
+{
+    public class PublicKey : ICredentials
+    {
+        public string Filename { get; set; }
+
+        public PublicKey(string filename)
+        {
+            this.Filename = filename;
+        }
+
+        public string GetScheme()
+        {
+            return "Key";
+        }
+
+        public string GetToken()
+        {
+            var publicKey = File.ReadAllText(Filename);
+            var pos = Filename.LastIndexOf('.');
+            var accessKey = Filename.Substring(0, pos);
+            var payload = new Dictionary<string, object>()
+                {
+                    { "iss" , "SinoPac_client" },
+                    { "exp", DateTimeOffset.Now.ToUnixTimeSeconds() + 30 },
+                    { "iat" , DateTimeOffset.Now.ToUnixTimeSeconds() },
+                    { "jti" , Guid.NewGuid().ToString() }
+                };
+
+            publicKey = publicKey.Replace("\n", "");
+            publicKey = publicKey.Replace("\r", "");
+            publicKey = publicKey.Replace("-----BEGIN PUBLIC KEY-----", "");
+            publicKey = publicKey.Replace("-----END PUBLIC KEY-----", "");
+            var publicKeyBytes = Convert.FromBase64String(publicKey);
+
+            MemoryStream stream = new MemoryStream(publicKeyBytes);
+            X509EncodedPublicKeyImporter importer = X509EncodedPublicKeyImporter.ImportFromDER(stream);
+            CngKey rsaKey = ((RSAPublicKeyImporter)importer).ToCngKey();
+
+            var extraHeaders = new Dictionary<string, object>();
+            extraHeaders.Add("kid", accessKey);
+
+            return JWT.Encode(payload, rsaKey, JweAlgorithm.RSA_OAEP_256, JweEncryption.A128GCM, extraHeaders: extraHeaders);
+        }
+    }
+}
---
+++ examples/src/main/cs/ApiTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OrbisAPI;
+
+namespace ApiTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            API api = new API();
+            api.Credentials = new PublicKey("1c71d432-06c6-4940-9a50-5d617a0c259b.pem");
+            api.Hostname = "https://portal.orbisfn.com";
+
+            Console.WriteLine(api.Get("/quotes/equity", "symbols", "msft"));
+            Console.ReadLine();
+        }
+    }
+}
