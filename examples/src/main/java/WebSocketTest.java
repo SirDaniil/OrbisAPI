0da34dfd0b19c085be4e2233fe72542ee1d12e7c
Daniil S <daniil@orbisfn.com>
2019-02-23 21:26:50 -0500

    UI support

--- examples/src/main/java/WebSocketTest.java
+++ examples/src/main/java/WebSocketTest.java
@@ -1,6 +1,9 @@
+import java.awt.*;
 import java.io.*;
 import java.nio.*;
+import java.text.*;
 import java.util.*;
+import javax.swing.*;
 import com.github.sd.*;
 import org.bson.*;
 import org.java_websocket.client.*;
@@ -16,10 +19,24 @@ public class WebSocketTest implements OrbisApiClient
         private BasicBSONDecoder decoder = new BasicBSONDecoder();
         private String[] symbols;
         private WebSocketClient ws;
+        private DateFormat hhmmssS = new SimpleDateFormat("HH:mm:ss.SSS");
+        private Map<String, UI> uis = new HashMap<>();
+        private boolean ui;
 
         public static void main(String[] args) throws Exception
             {
-                String settings = args.length > 0 ? args[0] : "settings.conf";
+                boolean ui = false;
+                String settings = "settings.conf";
+
+                for (String arg : args)
+                    if ("-ui".equalsIgnoreCase(arg))
+                        ui = true;
+                    else if (arg.length() > 0 && arg.charAt(0) != '-')
+                        settings = arg;
+
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+
                 Properties props = new Properties();
                 try (InputStream in = new FileInputStream(settings))
                     {
@@ -31,6 +48,7 @@ public class WebSocketTest implements OrbisApiClient
                 api.setCredentials(new PublicKeyCredentials(props.getProperty("key.file")));
 
                 WebSocketTest test = new WebSocketTest();
+                test.ui = ui;
                 test.symbols = props.getProperty("symbols").split(",");
                 test.ws = api.openWebSocket(test);
             }
@@ -71,5 +89,94 @@ public class WebSocketTest implements OrbisApiClient
                 Date timestamp = (Date) object.get("ts");
                 long lag = (System.currentTimeMillis() - timestamp.getTime());
                 System.out.println("(" + lag + ") " + object);
+
+                if (!ui)
+                    return;
+
+                if (object.containsField("u"))
+                    {
+                        BSONObject update = (BSONObject)object.get("u");
+                        for (String symbol : update.keySet())
+                            {
+                                UI ui = uis.computeIfAbsent(symbol, UI::new);
+
+                                BSONObject fields = (BSONObject) update.get(symbol);
+
+                                if (object.containsField("ts"))
+                                    ui.lbSentTimestamp.setText(hhmmssS.format(object.get("ts")));
+
+                                if (fields.containsField("px"))
+                                    ui.lbPrice.setText(fields.get("px").toString());
+
+                                if (fields.containsField("bx"))
+                                    ui.lbBid.setText(fields.get("bx").toString());
+
+                                if (fields.containsField("ax"))
+                                    ui.lbAsk.setText(fields.get("ax").toString());
+
+                                if (fields.containsField("bz"))
+                                    ui.lbBidSize.setText(fields.get("bz").toString());
+
+                                if (fields.containsField("az"))
+                                    ui.lbAskSize.setText(fields.get("az").toString());
+
+                                if (fields.containsField("ts"))
+                                    ui.lbQuoteTimestamp.setText(hhmmssS.format(fields.get("ts")));
+
+                                if (fields.containsField("rts"))
+                                    ui.lbReceivedTimestamp.setText(hhmmssS.format(fields.get("rts")));
+
+                                if (fields.containsField("dts"))
+                                    ui.lbDisptachedTimestamp.setText(hhmmssS.format(fields.get("dts")));
+
+                                ui.lbNowTimestamp.setText(hhmmssS.format(new Date()));
+                            }
+                    }
+            }
+
+        class UI extends JFrame
+            {
+                private JLabel lbBid = new JLabel();
+                private JLabel lbBidSize = new JLabel();
+                private JLabel lbAsk = new JLabel();
+                private JLabel lbAskSize = new JLabel();
+                private JLabel lbPrice = new JLabel();
+                private JLabel lbSentTimestamp = new JLabel();
+                private JLabel lbReceivedTimestamp = new JLabel();
+                private JLabel lbDisptachedTimestamp = new JLabel();
+                private JLabel lbQuoteTimestamp = new JLabel();
+                private JLabel lbNowTimestamp = new JLabel();
+
+                UI(String symbol) throws HeadlessException
+                    {
+                        JPanel panel = new JPanel(new GridLayout(10, 2, 2, 2));
+                        panel.setBorder(BorderFactory.createTitledBorder(" " + symbol + " "));
+                        panel.add(new JLabel("Price:"));
+                        panel.add(lbPrice);
+                        panel.add(new JLabel("Bid:"));
+                        panel.add(lbBid);
+                        panel.add(new JLabel("BidSz:"));
+                        panel.add(lbBidSize);
+                        panel.add(new JLabel("Ask:"));
+                        panel.add(lbAsk);
+                        panel.add(new JLabel("AskSz:"));
+                        panel.add(lbAskSize);
+                        panel.add(new JLabel("Quoted on:"));
+                        panel.add(lbQuoteTimestamp);
+                        panel.add(new JLabel("Q received on:"));
+                        panel.add(lbReceivedTimestamp);
+                        panel.add(new JLabel("Q dispatched on:"));
+                        panel.add(lbDisptachedTimestamp);
+                        panel.add(new JLabel("WS sent on:"));
+                        panel.add(lbSentTimestamp);
+                        panel.add(new JLabel("Local:"));
+                        panel.add(lbNowTimestamp);
+
+                        JFrame frame = new JFrame();
+                        frame.setTitle(symbol);
+                        frame.add(panel);
+                        frame.pack();
+                        frame.setVisible(true);
+                    }
             }
     }
