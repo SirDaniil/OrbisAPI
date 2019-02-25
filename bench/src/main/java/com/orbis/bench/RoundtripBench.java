c56d0858e973464918d3494acceb63a8c0f99839
Daniil S <daniil@orbisfn.com>
2019-02-24 21:15:41 -0500

    toys

--- bench/src/main/java/com/orbis/bench/RoundtripBench.java
+++ bench/src/main/java/com/orbis/bench/RoundtripBench.java
@@ -6,6 +6,7 @@ import java.util.*;
 import com.github.sd.*;
 import com.mongodb.*;
 import com.mongodb.client.model.*;
+import com.orbis.util.*;
 import org.bson.*;
 import org.java_websocket.client.*;
 import org.java_websocket.handshake.*;
@@ -47,9 +48,11 @@ public class RoundtripBench implements OrbisApiClient
                 api.setHostname(props.getProperty("hostname"));
                 api.setCredentials(new PublicKeyCredentials(props.getProperty("key.file")));
 
-                var test = new RoundtripBench();
-                test.props = props;
-                test.ws = api.openWebSocket(test);
+                var bench = new RoundtripBench();
+                bench.props = props;
+                bench.ws = api.openWebSocket(bench);
+
+                new Thread(bench::beginBench).start();
             }
 
         @Override
@@ -63,8 +66,6 @@ public class RoundtripBench implements OrbisApiClient
                 System.out.println("(+) Subscribing: " + sub);
 
                 ws.send(sub.toString());
-
-                new Thread(this::beginBench).start();
             }
 
         @Override
@@ -88,22 +89,27 @@ public class RoundtripBench implements OrbisApiClient
             {
                 var now = System.currentTimeMillis();
                 var object = decoder.readObject(bytes.array());
-                System.out.println(object);
 
-                if (object.containsField("u"))
-                    {
-                        BSONObject update = (BSONObject) object.get("u");
-                        for (String symbol : update.keySet())
-                            {
-                                BSONObject fields = (BSONObject) update.get(symbol);
+                if (!object.containsField("u"))
+                    return;
 
-                                if (!fields.containsField("ts"))
-                                    return;
+                BSONObject update = (BSONObject) object.get("u");
+                BSONObject fields = (BSONObject) update.get(markId);
+                if (fields == null)
+                    return;
 
-                                Date date = (Date)fields.get("ts");
-                                System.out.println("Roundtrip time: " + (now - date.getTime()) / 1000.0);
-                            }
-                    }
+                if (!fields.containsField("ts"))
+                    return;
+
+                if (!fields.containsField("px"))
+                    return;
+
+                Date date = (Date) fields.get("ts");
+                String price = fields.get("px").toString();
+                if (!Verhoeff.validateVerhoeff(price))
+                    return;
+
+                System.out.println("Roundtrip time: " + (now - date.getTime()) / 1000.0);
             }
 
         private void beginBench()
@@ -119,11 +125,18 @@ public class RoundtripBench implements OrbisApiClient
                 col.deleteOne(eq("_id", markId));
                 System.out.println("(*) Mark cleared");
 
+                long index = 1;
                 while (true)
                     try
                         {
-                            //col.updateOne(eq("_id", markId), combine(set("QuoteTime", new Date()), set("LastPx", Math.random())), new UpdateOptions().upsert(true));
-                            col.updateOne(eq("_id", markId), set("QuoteTime", new Date()), new UpdateOptions().upsert(true));
+                            col.updateOne(
+                                    eq("_id", markId),
+                                    combine(
+                                            set("QuoteTime", new Date()),
+                                            set("LastPx", Long.parseLong(Verhoeff.generateVerhoeff(index++)))
+                                    ),
+                                    new UpdateOptions().upsert(true)
+                            );
                             System.out.println("(*) Mark set on [" + new Date() + "]");
                             Thread.sleep(1000);
                         }
