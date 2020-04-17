7367a86059e132ace9d3a80ea73533f0600cebd3
SirDaniil <daniil@orbisfn.com>
2020-04-17 11:06:39 -0400

    drift testing

--- advisory/src/main/java/com/orbis/advisory/DriftTest.java
+++ advisory/src/main/java/com/orbis/advisory/DriftTest.java
@@ -2,6 +2,7 @@ package com.orbis.advisory;
 
 import java.io.*;
 import com.github.sd.*;
+import org.json.*;
 import static com.orbis.advisory.AdvisoryEndpoints.*;
 
 /**
@@ -13,6 +14,27 @@ public class DriftTest extends Advisory
         public static void main(String[] args) throws IOException
             {
                 OrbisAPI api = createAPI();
-                print(api.get(AdvisoryModelDrift, "{modelId}", 1));
+                JSONArray allocations = api.get(AdvisoryModelDrift, "{modelId}", 1);
+                print(allocations);
+                for (int i = 0; i < allocations.length(); i++)
+                    {
+                        JSONObject allocation = allocations.getJSONObject(i);
+                        JSONObject quote = allocation.getJSONObject("quote");
+                        JSONArray targets = allocation.getJSONArray("targets");
+
+                        print("Quote: " + quote.getString("symbol"));
+                        print("Transaction: " + allocation.getString("transaction"));
+                        print("Quantity: " + allocation.getDouble("quantity"));
+                        for (int j = 0; j < targets.length(); j++)
+                            {
+                                JSONObject target = targets.getJSONObject(j);
+                                print("\tAccount: " + target.getJSONObject("account"));
+                                print("\tCurrent: " + target.optDouble("currentPct") * 100 + "%");
+                                print("\tExpected: " + target.optDouble("expectedPct") * 100 + "%");
+                                print("\tBalance: " + target.optJSONObject("rtb"));
+                                print("\tDrift: " + target.optDouble("drift") * 100 + "%");
+                                print("\tQty: " + target.getDouble("quantity"));
+                            }
+                    }
             }
     }
