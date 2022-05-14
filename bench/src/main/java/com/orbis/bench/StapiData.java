ae07b95c44bf89c5334cc621dec836224b5e60e4
Daniil S <daniil@orbisfn.com>
2022-05-14 13:39:34 -0400

    roundtrip test

--- bench/src/main/java/com/orbis/bench/StapiData.java
+++ bench/src/main/java/com/orbis/bench/StapiData.java
@@ -9,7 +9,7 @@ public record StapiData(double seconds)
                 var obj = new JSONObject();
                 obj.put("seconds", seconds);
                 obj.put("tag", "audit-quotes");
-                obj.put("group", "MUMBAY");
+                obj.put("group", "audit-MUMBAY");
                 obj.put("name", "ping");
                 obj.put("timestamp", System.currentTimeMillis());
 
